641f3e42dc65ffd1f8d24bb1900184d850daa08b
Vlad Polyak <v76859822@gmail.com>
2026-04-16 09:17:23 +0300

    remove vite pwa plugin

--- snake-app/vite.config.ts
+++ snake-app/vite.config.ts
@@ -1,33 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-    plugins: [
-        react(),
-        VitePWA({
-            registerType: "autoUpdate",
-            manifest: {
-                name: "Snake Game",
-                short_name: "Snake",
-                description: "Snake game",
-                theme_color: "#111111",
-                background_color: "#111111",
-                display: "standalone",
-                start_url: "/",
-                icons: [
-                    {
-                        src: "/pwa-192x192.png",
-                        sizes: "192x192",
-                        type: "image/png",
-                    },
-                    {
-                        src: "/pwa-512x512.png",
-                        sizes: "512x512",
-                        type: "image/png",
-                    },
-                ],
-            },
-        }),
-    ],
+    plugins: [react()],
 });
